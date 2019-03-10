61e5224afaaae053442d3c0cdb4e9a3377995d8e
trarck <trarck@gmail.com>
2019-03-10 17:29:22 +0800

    测试解析CellPosition.

---
+++ Test/CellPositionTest.cs
@@ -0,0 +1,83 @@
+using NUnit.Framework;
+using TK.ExcelData;
+
+namespace Tests
+{
+    public class CellPositionTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            ReadLinkHelper.CellPosition cp = ReadLinkHelper.GetCellPosition("A1B2");
+            Assert.AreEqual(cp.colStart, 0);
+            Assert.AreEqual(cp.colEnd, 1);
+            Assert.AreEqual(cp.rowStart, 0);
+            Assert.AreEqual(cp.rowEnd, 1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            ReadLinkHelper.CellPosition cp = ReadLinkHelper.GetCellPosition("A1,B2");
+            Assert.AreEqual(cp.colStart, 0);
+            Assert.AreEqual(cp.colEnd, 1);
+            Assert.AreEqual(cp.rowStart, 0);
+            Assert.AreEqual(cp.rowEnd, 1);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            ReadLinkHelper.CellPosition cp = ReadLinkHelper.GetCellPosition("1B2");
+            Assert.AreEqual(cp.colStart,0);
+            Assert.AreEqual(cp.colEnd, 1);
+            Assert.AreEqual(cp.rowStart, 0);
+            Assert.AreEqual(cp.rowEnd, 1);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            ReadLinkHelper.CellPosition cp = ReadLinkHelper.GetCellPosition("1,B2");
+            Assert.AreEqual(cp.colStart, 0);
+            Assert.AreEqual(cp.colEnd, 1);
+            Assert.AreEqual(cp.rowStart, 0);
+            Assert.AreEqual(cp.rowEnd, 1);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            ReadLinkHelper.CellPosition cp = ReadLinkHelper.GetCellPosition("1B");
+            Assert.AreEqual(cp.colStart, 0);
+            Assert.AreEqual(cp.colEnd, 1);
+            Assert.AreEqual(cp.rowStart, 0);
+            Assert.AreEqual(cp.rowEnd, -1);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            ReadLinkHelper.CellPosition cp = ReadLinkHelper.GetCellPosition(",B");
+            Assert.AreEqual(cp.colStart, 0);
+            Assert.AreEqual(cp.colEnd, 1);
+            Assert.AreEqual(cp.rowStart, 0);
+            Assert.AreEqual(cp.rowEnd, -1);
+        }
+
+        [Test]
+        public void Test7()
+        {
+            ReadLinkHelper.CellPosition cp = ReadLinkHelper.GetCellPosition("C4F8");
+            Assert.AreEqual(cp.colStart, 2);
+            Assert.AreEqual(cp.colEnd, 5);
+            Assert.AreEqual(cp.rowStart,3);
+            Assert.AreEqual(cp.rowEnd, 7);
+        }
+    }
+}
